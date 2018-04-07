ab56285f873fc36afcdf04e08980ef33f8d8042c
HALEESSURFACE\rytig <ryanmasonjar@gmail.com>
2018-04-07 15:25:17 -0400

    Create layout

---
+++ src/components/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+
+    <div class="row text-center">
+      <div class="col-md-12 spacer">
+        <div class="mb-3">
+          <div class="text-dark">
+            <img src="https://image.ibb.co/fqLKsH/IMG_1296_550x210.png" class="mt-4 image">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row text-center mt-4 pt-4 spacer">
+      <div class="col-md-6 col-sm-12 ">
+        <div class="mb-3">
+          <div class="text-dark">
+            <h4 class="card-title">What is Open Access</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <img src="https://image.ibb.co/esa8mc/unlocked_padlock.png">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <div class=" mb-3" >
+          <div class="text-dark">
+            <h4 class="card-title">Why Open Access</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <img src="https://image.ibb.co/d4Nomc/keyword.png">
+          </div>
+        </div>
+      </div>
+    </div>
+
+   <hr>
+
+    <div class="row text-center spacer">
+      <div class="col-md-12">
+        <div class="mb-3">
+          <div class="text-dark">
+            <h4 class="card-title">Steps you can take for Open Access</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="row justify-content-center ">
+              <div class="col-6">
+                <ol class="text-left">
+                  <li>Have a conversation with a colleague</li>
+                  <li>Find a Knowledgeable person in your area</li>
+                  <li>Hold an Open Access event</li>
+                  <li>Post a preprint before submission</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'This is the home page.'
+    }
+  }
+}
+</script>
